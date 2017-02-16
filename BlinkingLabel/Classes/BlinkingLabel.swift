ffdf28c9ab992bed2a34faf197941f0951118f28
Weeraphat Kaewnang <weeraphat@databerries.com>
2017-02-16 16:45:55 +0100

    Cocoapods tuto

---
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -0,0 +1,26 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by Weeraphat Kaewnang on 2/16/17.
+//
+//
+
+import Foundation
+import UIKit
+
+public class BlinkingLabel : UILabel {
+    
+    public func startBlinking() {
+//        let options: UIViewAnimationOptions = [.repeat, .autoreverse]
+//        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+//            self.alpha = 0
+//        }, completion: nil)
+        print("Start blinking !!")
+    }
+    
+    public func stopBlinking() {
+//        alpha = 1
+//        layer.removeAllAnimations()
+    }
+}
